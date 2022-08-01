5227c7b5f7d92d05f9770d4a298cd8a606566005
Emerson Millard <emillard.code@gmail.com>
2022-08-01 12:00:00 -0700

    Added entity class.

---
+++ src/main/java/com/project/entity/PurchaseOrder.java
@@ -0,0 +1,30 @@
+package com.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PURCHASE_ORDER_TBL")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PurchaseOrder {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private Integer userId;
+    private Integer productId;
+    private Integer price;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+}
