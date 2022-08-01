3f1cea3921b4e6f44faa4bf01bb31b9bb30a46c2
Emerson Millard <emillard.code@gmail.com>
2022-08-01 12:00:00 -0700

    Updated service class.

--- src/main/java/com/project/service/OrderStatusPublisher.java
+++ src/main/java/com/project/service/OrderStatusPublisher.java
@@ -1,4 +1,20 @@
 package com.project.service;
 
+import com.project.dto.OrderRequestDTO;
+import com.project.event.OrderEvent;
+import com.project.event.OrderStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Sinks;
+
 public class OrderStatusPublisher {
+
+    @Autowired
+    private Sinks.Many<OrderEvent> orderSinks;
+
+    public void publishOrderEvent(OrderRequestDTO orderRequestDto, OrderStatus orderStatus){
+        OrderEvent orderEvent = new OrderEvent(orderRequestDto, orderStatus);
+        orderSinks.tryEmitNext(orderEvent);
+
+    }
+
 }
