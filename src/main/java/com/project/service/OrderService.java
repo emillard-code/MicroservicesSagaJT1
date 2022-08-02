0377ccc5619148e47616905d4d625486639543c7
Emerson Millard <emillard.code@gmail.com>
2022-08-02 12:00:00 -0700

    Added annotation.

--- src/main/java/com/project/service/OrderService.java
+++ src/main/java/com/project/service/OrderService.java
@@ -5,10 +5,12 @@ import com.project.entity.PurchaseOrder;
 import com.project.event.OrderStatus;
 import com.project.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class OrderService {
 
     @Autowired
