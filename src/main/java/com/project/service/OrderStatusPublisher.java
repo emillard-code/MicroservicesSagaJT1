5ca275c056ace4e0870517f2ee36a7b81af798f6
Emerson Millard <emillard.code@gmail.com>
2022-08-02 12:00:00 -0700

    Added annotation.

--- src/main/java/com/project/service/OrderStatusPublisher.java
+++ src/main/java/com/project/service/OrderStatusPublisher.java
@@ -4,14 +4,17 @@ import com.project.dto.OrderRequestDTO;
 import com.project.event.OrderEvent;
 import com.project.event.OrderStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Sinks;
 
+@Service
 public class OrderStatusPublisher {
 
     @Autowired
     private Sinks.Many<OrderEvent> orderSinks;
 
     public void publishOrderEvent(OrderRequestDTO orderRequestDto, OrderStatus orderStatus){
+
         OrderEvent orderEvent = new OrderEvent(orderRequestDto, orderStatus);
         orderSinks.tryEmitNext(orderEvent);
 
