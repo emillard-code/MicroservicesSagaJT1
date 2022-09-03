1cf7d82e0992d6e83bd672756463e273aeb39ae0
Emerson Millard <emillard.code@gmail.com>
2022-09-03 13:23:03 -0700

    Initial commit

---
+++ src/test/java/com/project/microservicessagajt1/MicroservicesSagaJt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.microservicessagajt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesSagaJt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
